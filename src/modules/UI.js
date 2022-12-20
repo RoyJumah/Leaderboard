3879b85ea3dad2ed42b575234b720633be5ed94a
RoyJumah <royjumah17@gmail.com>
2022-12-20 13:09:07 +0300

    Collect data from the form

---
+++ src/modules/UI.js
@@ -0,0 +1,23 @@
+import LeaderboardStore from './leadersData.js';
+
+export default class UI {
+  static addLeader(leader) {
+    const leaderRank = document.getElementById('leaderboard');
+    const ul = document.createElement('ul');
+    ul.classList.add('unordered-list');
+    ul.innerHTML = `
+       <p><b>${leader.name}</b> :</p>
+      <p><b>${leader.score}</b></p>
+    
+      `;
+
+    leaderRank.appendChild(ul);
+  }
+
+  static displayLeader() {
+    const leaders = LeaderboardStore.getLeaders();
+    leaders.forEach(leader => {
+      UI.addLeader(leader);
+    });
+  }
+}
---
+++ src/modules/leaders.js
@@ -0,0 +1,6 @@
+export default class Leaderboard {
+  constructor(name, score) {
+    this.name = name;
+    this.score = score;
+  }
+}
