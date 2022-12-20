1e594574f374d492b4eee8ee85f6fc240c016efd
RoyJumah <royjumah17@gmail.com>
2022-12-20 13:11:11 +0300

    Create function to reload data from storage once the page is loaded

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+import './style.css';
+import Leaderboard from './modules/leaders.js';
+import UI from './modules/UI.js';
+import LeaderboardStore from './modules/leadersData.js';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const name = form.name.value;
+  const score = form.score.value;
+
+  const leader = new Leaderboard(name, score);
+  UI.addLeader(leader);
+  LeaderboardStore.addLeader(leader);
+
+  form.reset();
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  UI.displayLeader();
+});
